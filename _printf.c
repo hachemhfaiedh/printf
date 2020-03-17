13b90645470233ca1506f44c63639c186d130171
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-17 22:43:08 +0000

    betty fix

--- _printf.c
+++ _printf.c
@@ -8,27 +8,28 @@ int _printf(const char *format, ...)
 {
 	int i = 0, count_format = 0;
 	va_list pa;
+
 	va_start(pa, format);
-	if(format == NULL)
+	if (format == NULL)
 	return (-1);
-	for(i = 0;format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-	if ( format[i] == '%')
+		if (format[i] == '%')
 		{
 			count_format++;
-			for(;format[i] == ' '; i++)
+			for (; format[i] == ' '; i++)
 			{
-			}
+				}
 			if (format[i] == '\0')
 				return (-1);
 			count_format += get_specifier_functions(format[i + 1], pa) - 1;
 			i = i + 1;
 		}
-	else
-	{
-		_putchar(format[i]);
-		count_format++;
-	}
+		else
+		{
+			_putchar(format[i]);
+			count_format++;
+		}
 	}
 	va_end(pa);
 	return (count_format);
