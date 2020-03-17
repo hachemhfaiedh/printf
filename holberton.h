7dbc380663f9075af66d12dab8ab1447294235dd
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-03-17 15:02:54 +0000

    _printf

--- holberton.h
+++ holberton.h
@@ -3,5 +3,9 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _char(va_list spec);
+int _string(va_list spec);
+int _percentage(__attribute__((unused))va_list spec);
+int _integer(va_list spec);
 
 #endif
